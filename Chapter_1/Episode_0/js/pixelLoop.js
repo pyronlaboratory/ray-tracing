668ac43b9e5492e8ffab3cc23f6b00fc6a4aa224
Ronnie <ronnie94official@gmail.com>
2023-11-02 22:48:08 +0000

    Update pixelLoop.js

--- Chapter_1/Episode_0/js/pixelLoop.js
+++ Chapter_1/Episode_0/js/pixelLoop.js
@@ -17,19 +17,12 @@ let r = 0;
 let g = 0;
 let b = 0;
 
-// loop over all pixels on screen
 for (let row = 0; row < canvas.height; row++)
 {
 	for (let column = 0; column < canvas.width; column++)
 	{
 		canvasX = column / (canvas.width - 1);
 		canvasY = row / (canvas.height - 1);
-		// if (row == 0)
-		// {
-		// 	console.log("canvasX is " + canvasX);
-		// }
-
-		// canvasX goes from 0.0 to 1.0
 
 		r = canvasX * 255;
 		r = Math.floor(r);
@@ -40,9 +33,7 @@ for (let row = 0; row < canvas.height; row++)
 		b = (1 - canvasX) * 255;
 		b = Math.floor(b);
 
-		//"rgb(r,g,b)"
 		context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
 		context.fillRect(column, row, 1, 1);
 	}
-	///console.log("canvasY is " + canvasY);
 }
